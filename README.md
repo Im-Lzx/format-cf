a6c1a2435c290ca2149c2c8a4ec95de424895c5e
lizhaoxin <zhaoxin.li@uhomes.com>
2022-07-16 17:52:25 +0800

    project init

---
+++ README.md
@@ -0,0 +1,20 @@
+### develop配置项文件
+
+安装
+```js
+npm install @lzx-npm/format-cf --save-dev
+```
+
+vscode 开启插件
+```js
+eslint
+vetur
+prettier
+```
+
+tips: 目前属于试验阶段存在配置冲突，待解决
+
+
+future:
+- eslint 对于小程序、vue项目单独生成配置项 
+- and more ...
---
+++ test/proj-settings/bin/proj-settings-copy.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+
+let currentPath = __dirname;
+let libsPath = path.resolve(__dirname, '../lib');
+
+/**
+ * 查询当前项目根package.json所在的路径
+ */
+function queryProjectPackagesJsonPath() {
+    currentPath = path.resolve(currentPath, '..');
+    fs.exists(`${currentPath}/package.json`, function (exists) {
+        if (exists) {
+            console.log('文件存在');
+            runCopy(currentPath);
+        }
+
+        if (!exists) {
+            queryProjectPackagesJsonPath();
+        }
+    });
+}
+
+/**
+ * 进行文件copy
+ * @param {string} projPath package.json所在的路径
+ */
+function runCopy(projPath) {
+    let files = fs.readdirSync(libsPath);
+
+    files.forEach((ele) => {
+        let orginFile = path.resolve(libsPath, ele);
+        let targetFile = path.resolve(projPath, ele);
+        fs.copyFile(orginFile, targetFile, function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            console.log(`copy ${ele} 成功！`);
+        });
+    });
+}
+
+/**
+ * 文件开始执行
+ */
+function start() {
+    queryProjectPackagesJsonPath();
+}
+
+start();
